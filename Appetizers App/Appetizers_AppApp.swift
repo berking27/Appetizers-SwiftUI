467720dada577aa0558a7dc0ef816057b5b19f97
Berkin KOCA <bekoca@innova.com.tr>
2024-03-13 14:18:04 +0300

    Initial Commit

---
+++ Appetizers App/Appetizers_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizers_AppApp.swift
+//  Appetizers App
+//
+//  Created by Berkin KOCA on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Appetizers_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
