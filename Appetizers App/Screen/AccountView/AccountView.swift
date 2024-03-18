9270b280cbb2dd6956c6bfbac04dc73c616d21ea
Berkin KOCA <bekoca@innova.com.tr>
2024-03-18 17:41:33 +0300

    Account View UI setup is done in form format

--- Appetizers App/Screen/AccountView/AccountView.swift
+++ Appetizers App/Screen/AccountView/AccountView.swift
@@ -8,10 +8,55 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationStack {
-            Text("Account")
-                .navigationTitle("Acount")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled()
+                    
+                    DatePicker("Birthday", 
+                               selection: $birthDate,
+                               displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                    
+                } header: {
+                    Text("Personal Info")
+                }
+                
+                Section {
+                    Toggle(isOn: $extraNapkins) {
+                        Text("Extra Napkins")
+                    }
+                    
+                    Toggle(isOn: $frequentRefills) {
+                        Text("Frequent Refills")
+                    }
+                    
+                } header: {
+                    Text("Requests")
+                }
+                .tint(.brandPrimary)
+
+            }
+            .navigationTitle("👤 Acount")
         }
     }
 }
