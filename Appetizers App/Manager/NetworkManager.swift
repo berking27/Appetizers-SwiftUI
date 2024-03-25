7fb70990b872877456a7d1af3aa57dc67aabc935
Berkin KOCA <bekoca@innova.com.tr>
2024-03-25 17:36:50 +0300

    Regex updates handled. Some guard let statements updated

--- Appetizers App/Manager/NetworkManager.swift
+++ Appetizers App/Manager/NetworkManager.swift
@@ -53,7 +53,7 @@ final class NetworkManager {
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, _ in
             
-            guard let data = data, let image = UIImage(data: data) else {
+            guard let data, let image = UIImage(data: data) else {
                 completion(nil)
                 return
             }
